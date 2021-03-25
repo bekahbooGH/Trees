d28751df03a0a064ed463fce254696913e1a98c9
Bekah McCurry <rebekah.a.mccurry@icloud.com>
2021-03-25 16:53:04 -0700

    Add repr method, correct is_descendant, add is_ancestor.

--- trees.py
+++ trees.py
@@ -5,8 +5,9 @@ class Node():
         self.data = data
         self.children = children or []
 
-    # def __repr__(self):
-        # implement a repr method for testing
+    def __repr__(self):
+        return (
+        f'Node data: {self.data}, Node children: {self.children}')
 
     # Add child
     def add_child(self, node):
@@ -15,7 +16,7 @@ class Node():
     # print children
     def print_children(self):
         for child in self.children:
-            print(f'Child of {self.data}: ', child.data)
+            print(f'Node {child.data} is child of Node {self.data}')
 
     def print_descendants(self):
         # create list for nodes to check instantiate it with current nodes children
@@ -25,7 +26,7 @@ class Node():
             # pop off front of list to print value
             curr = nodes_to_check.pop(0)
             # print value
-            print('Child of Parent 1', curr.data)
+            print(f'Node {curr.data} is descendant of Node {self.data}')
             # check if node has any children
             if curr.children:  # [] == false
                 # add children to front of nodes to check list
@@ -33,7 +34,21 @@ class Node():
                     # insert each child at front of list
                     nodes_to_check.insert(0, child)
 
-    def is_descendant(self, data):
+    def is_descendant(self, node):
+        nodes_to_check = node.children.copy()
+
+        while nodes_to_check:
+            curr = nodes_to_check.pop()
+            if curr == self:
+                print('True')
+                return
+            else:
+                if curr.children:
+                    for child in curr.children:
+                        nodes_to_check.append(child)
+        print('False')
+
+    def is_ancestor(self, data):
         nodes_to_check = self.children.copy()
 
         while nodes_to_check:
@@ -66,6 +81,7 @@ node6.add_child(node7)
 node3.add_child(node8)
 node1.add_child(node3)
 
-
-node1.print_descendants()
-node3.is_descendant(10)  # True
+# node1.print_children()
+# node2.print_descendants()
+node3.is_ancestor(9)  # True
+node8.is_descendant(node3)
