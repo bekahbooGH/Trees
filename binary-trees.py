6f2e4c30f66623f345a94d757bd22b69e6113c66
Bekah McCurry <rebekah.a.mccurry@icloud.com>
2021-03-27 13:34:29 -0700

    Corrected binary-trees method.

--- binary-trees.py
+++ binary-trees.py
@@ -14,19 +14,19 @@ class Binary_Node():
         if self.right:
             self.right.descendants_preorder()
 
-    def descendants_inorder(self, node):
-        if node.left:
-            descendants_inorder(node.left)
-        print(node.data)
-        if node.right:
-            descendants_inorder(node.right)
+    def descendants_inorder(self):
+        if self.left:
+            self.left.descendants_inorder()
+        print(self.data)
+        if self.right:
+            self.right.descendants_inorder()
     
-    def descendants_postorder(self, node):
-        if node.left:
-            descendants_postorder(node.left)
-        if node.right:
-            descendants_postorder(node.right)
-        print(node.data)
+    def descendants_postorder(self):
+        if self.left:
+            self.left.descendants_postorder()
+        if self.right:
+            self.right.descendants_postorder()
+        print(self.data)
 
     
 # OUTPUT: 7 1 34 2 0
@@ -36,18 +36,55 @@ class Binary_Node():
     def count_nodes(self):
         count = 1  # Line 1
         if self.left:  # Line 2
-            count += count_nodes(self.left)  # Line 3
+            count += self.left.count_nodes()  # Line 3
         if self.right:  # Line 4
-            count += count_nodes(self.right)  # Line 5
+            count += self.right.count_nodes()  # Line 5
         return count  # Line 6
 
-    def preorderTraversal(self, root):
-    #  -> List[int]:
-        # return the preorder traversal of its nodes' values.
-            if not root:
-                return []
-            if root:
-                return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+class Binary_Tree():
+
+    def __init__(self, root=None):
+        self.root = root
+
+    def add_node(self, node):
+        # first check to see if the root of our tree is empty
+        if self.root == None:
+            self.root = node
+            return
+        else:
+            def check_children(self, node):
+                if not self.left:
+                    self.left = node
+                    return
+                if not self.right:
+                    self.right = node
+                    return
+                check_children(self.left, node)
+            check_children(self.root, node)
+
+    # Replace a node
+    def replace_node(self, node_to_replace, new_node):
+
+        nodes_to_visit = [self.root]
+        # current = self.root
+        # [9]
+        while nodes_to_visit:
+            current = nodes_to_visit.pop(0)  # 9
+            if current.data == node_to_replace.data:  # 9 == 8
+                node_to_replace.data = new_node.data
+                return
+            else:
+                nodes_to_visit.extend([current.left, current.right])
+                print(nodes_to_visit)
+
+        # def preorderTraversal(self, root):
+    # #  -> List[int]:
+    #     # return the preorder traversal of its nodes' values.
+    #         if not root:
+    #             return []
+    #         if root:
+    #             return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
 
     def preorderTraversal(self, root):
     # -> List[int]:
@@ -76,43 +113,6 @@ class Binary_Node():
             fxn(root)
             return root_list
 
-
-class Binary_Tree():
-
-    def __init__(self, root=None):
-        self.root = root
-
-    def add_node(self, node):
-        # first check to see if the root of our tree is empty
-        if self.root == None:
-            self.root = node
-            return
-        else:
-            def check_children(parent=self.root):
-                if not parent.left:
-                    parent.left = node
-                    return
-                if not parent.right:
-                    parent.right = node
-                    return
-                check_children(parent.left)
-            check_children()
-
-    # Replace a node
-    def replace_node(self, node_to_replace, new_node):
-
-        nodes_to_visit = [self.root]
-        # current = self.root
-        # [9]
-        while nodes_to_visit:
-            current = nodes_to_visit.pop(0)  # 9
-            if current.data == node_to_replace.data:  # 9 == 8
-                node_to_replace.data = new_node.data
-                return
-            else:
-                nodes_to_visit.extend([current.left, current.right])
-                print(nodes_to_visit)
-
 node10 = Binary_Node(10)
 b_tree = Binary_Tree()
 b_tree.add_node(node10)
@@ -123,15 +123,15 @@ node8 = Binary_Node(8)
 b_tree.add_node(node8)
 node15 = Binary_Node(15)
 b_tree.add_node(node15)
-# b_tree.print_descendants()
-# b_tree.descendants_preorder(node10)
-# b_tree.descendants_inorder(node10)
-# b_tree.descendants_postorder(node10)
+node7 = Binary_Node(7)
+b_tree.add_node(node7)
 node10.descendants_preorder()
+print()
+node10.descendants_inorder()
+print()
+node10.descendants_postorder()
 
 # STACK
 # A: count_nodes(node7) count=5 Line 5
 # Return 5
-node7 = Binary_Node(7)
-b_tree.add_node(node7)
-node7.count_nodes()
+print(node5.count_nodes())
